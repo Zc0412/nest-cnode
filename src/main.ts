e5c85da995062321c0135ba7d59bab0f8b843a4e
less is more <18666270412@163.com>
2023-08-01 00:24:45 +0800

    feat:response Interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformResponseInterceptor } from './transform/transform-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Pipe
+  // Pipe 管道
   app.useGlobalPipes(new ValidationPipe());
+  // Interceptor 拦截器
+  app.useGlobalInterceptors(new TransformResponseInterceptor());
   // 监听端口
   await app.listen(process.env.PORT);
 }
