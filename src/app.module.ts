a10b03d312780df41c36e0e10bab4b63b9ec62a0
less is more <18666270412@163.com>
2023-08-17 23:29:05 +0800

    fix:typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,6 @@ import { TopicModule } from './topic/topic.module';
 import { Topic } from './topic/entities/topic.entity';
 import { UserAgent } from './user/entities/user-agent.entity';
 
-const { MYSQL_DATABASE, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_HOST } =
-  process.env;
-console.log(MYSQL_PASSWORD);
-
 @Module({
   imports: [
     // 配置.env
@@ -21,13 +17,13 @@ console.log(MYSQL_PASSWORD);
     // 配置typeorm
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: MYSQL_HOST,
+      host: process.env.DATABASE_HOST,
       port: 3306,
-      username: MYSQL_USERNAME,
-      password: MYSQL_PASSWORD || '123456',
-      database: MYSQL_DATABASE,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
       entities: [User, UserAgent, Topic],
-      synchronize: true,
+      // synchronize: true,
     }),
     UserModule,
     AuthModule,
