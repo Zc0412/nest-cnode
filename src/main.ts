3d7796ab78c7d37d54a633ef932033e7faec8ea1
less is more <18666270412@163.com>
2023-08-17 23:00:45 +0800

    feat:port

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   // hbs 模板引擎
   app.setViewEngine('hbs');
   // 监听端口
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
